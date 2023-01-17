bbb324bb926d0ec272bc933555dc6ded407afe1a
seonhan <seonhan.dev@gmail.com>
2023-01-17 14:11:08 +0900

    [info_toon] FutureBuilder를 이용하여 API 비동기작업 수정

--- info_toon/lib/main.dart
+++ info_toon/lib/main.dart
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
